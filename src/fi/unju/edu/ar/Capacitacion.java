e03a3dc3513d0ce71844b6dc115c6cbacec2ca95
Fabricio Bazan <112402233+fabri98bazan@users.noreply.github.com>
2024-04-03 17:56:00 -0300

    He inicializado el proyecto Close #1

---
+++ src/fi/unju/edu/ar/Capacitacion.java
@@ -0,0 +1,23 @@
+package fi.unju.edu.ar;
+
+public class Capacitacion {
+	private String nombre;
+
+	public Capacitacion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Capacitacion(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+}
